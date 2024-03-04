a59793ad06092f9aad15ac77639c3ad62f32a0f1
deepak <deepak.c@telliant.net>
2024-03-04 11:16:00 +0530

    custom query hooks

--- src/components/RQSuperHeroes.jsx
+++ src/components/RQSuperHeroes.jsx
@@ -1,9 +1,5 @@
 import { useQuery } from "react-query";
-import axios from "axios";
-
-const fetchSuperHeroes = () => {
-    return axios.get("http://localhost:8000/superheroes");
-}
+import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
 
 const RQSuperHeroes = () => {
 
@@ -15,7 +11,10 @@ const RQSuperHeroes = () => {
         console.log("Perform side effects onError :", error);
     }
 
-    const { data, isLoading, isError, error, isFetching, refetch } = useQuery("super-heroes", fetchSuperHeroes, {
+    // custom query hook
+    const {data, isLoading, isError, error, isFetching, refetch} = useSuperHeroesData(onSuccess, onError);
+
+    //const { data, isLoading, isError, error, isFetching, refetch } = useQuery("super-heroes", fetchSuperHeroes, {
         // cacheTime: 5000, // (default 5mins) cache results for 5sec
         //staleTime: 30000, // (default 0) fetch(make) request after 30sec
         // refetchOnMount: true, // (default true) fetch(make) request on component mount
@@ -23,12 +22,12 @@ const RQSuperHeroes = () => {
         // refetchInterval: false, // (default false) if true fetch(make) request on refetchInterval(in ms) interval when window tab is focus
         // refetchIntervalInBackground: false, // (default false) if true fetch(make) request on refetchIntervalInBackground(in ms) interval when window tab is not focus
         // enabled: false, // (default true) if false, query will not be executed and no error will be thrown. Useful for conditional query execution (onClick using refetch event).
-        onSuccess: onSuccess, // perform side effects on success.
-        onError: onError, // perform side effects on error.
-        select: (data) => { // for data transformation before returning to the component.
-            return data.data.map(heros => heros.name);
-        }
-    });
+        // onSuccess: onSuccess, // perform side effects on success.
+        // onError: onError, // perform side effects on error.
+        // select: (data) => { // for data transformation before returning to the component.
+        //     return data.data.map(heros => heros.name);
+        // }
+    //});
 
     console.log("isLoading :", isLoading, "isFetching :", isFetching)
 
---
+++ src/hooks/useSuperHeroesData.js
@@ -0,0 +1,17 @@
+import { useQuery } from "react-query"
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+    return axios.get("http://localhost:8000/superheroes");
+}
+
+
+export const useSuperHeroesData = (onSuccess, onError) => {
+    return useQuery('super-heroes', fetchSuperHeroes, {
+        onSuccess: onSuccess, // perform side effects on success.
+        onError: onError, // perform side effects on error.
+        select: (data) => { // for data transformation before returning to the component.
+            return data.data.map(heros => heros.name);
+        }
+    })
+}
